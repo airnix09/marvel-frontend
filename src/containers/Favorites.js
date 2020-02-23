4a944b89f24cd588bb301c5cd06faaf1c0d13c19
Hery R <airnix@yahoo.fr>
2020-02-23 21:30:22 +0100

    update favorites.js

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -26,14 +26,116 @@ const Favorites = () => {
   console.log("listFavComics", listFavComics);
   console.log("listFavCharacters", listFavCharacters);
 
+  // déclaration des states
+  const [dataChar, setDataChar] = useState([]);
+  const [dataCom, setDataCom] = useState([]);
+  const urlChar = "https://mybackend-marvel.herokuapp.com/characters/";
+  const urlCom = "https://mybackend-marvel.herokuapp.com/comics/";
+  const [isCharLoaded, setIsCharLoaded] = useState(false);
+  const [isComLoaded, setIsComLoaded] = useState(false);
+
+  // déclaration de la fonction qui recherche les données des personnages favoris
+  const fetchCharData = async () => {
+    try {
+      // pour chaque personnage dans les favoris
+      if (listFavCharacters.length > 0) {
+        for (let index = 0; index < listFavCharacters.length; index++) {
+          const elem = array[index];
+          // récupération des données sur le backend
+          const response = await axios.get(urlChar + elem);
+          console.log("retour du backend", response.data.results);
+          // on stock la réponse dans un état tableau
+          const copy = [...dataChar];
+          copy.push(response.data.results);
+          setDataChar(copy);
+        }
+      }
+      // on dit que la page est chargée
+      setIsCharLoaded(true);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // déclaration de la fonction qui recherche les données des comics favoris
+  const fetchComData = async () => {
+    try {
+      // pour chaque personnage dans les favoris
+      if (listFavCharacters.length > 0) {
+        for (let index = 0; index < listFavCharacters.length; index++) {
+          const elem = array[index];
+          // récupération des données sur le backend
+          const response = await axios.get(urlCom + elem);
+          console.log("retour du backend", response.data.results);
+          // on stock la réponse dans un état tableau
+          const copy = [...dataCom];
+          copy.push(response.data.results);
+          setDataCom(copy);
+        }
+      }
+      // on dit que la page est chargée
+      setIsComLoaded(true);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // lancement à la recharge de la page
+  useEffect(() => {
+    fetchCharData();
+    fetchComData();
+  }, []);
+
   return (
     <div className="favorites">
       <div className="container">
         <div className="favChar">
           <h2>Personnages favoris : </h2>
+          <div className="result">
+            {isCharLoaded && dataChar.length > 0 ? (
+              dataChar.map((elem, index) => {
+                return (
+                  <Card
+                    id={elem.id}
+                    key={elem.id}
+                    title={elem.name}
+                    picture={
+                      elem.thumbnail.path + "." + elem.thumbnail.extension
+                    }
+                    description={elem.description}
+                    setFavoriteCharacters={setFavoriteCharacters}
+                    favoriteCharacters={favoriteCharacters}
+                  />
+                );
+              })
+            ) : (
+              <div className="charging">Chargement en cours</div>
+            )}
+          </div>
         </div>
         <div className="favCom">
           <h2>Comics favoris :</h2>
+          <div className="result">
+            {isComLoaded && dataCom.length > 0 ? (
+              dataCom.map((elem, index) => {
+                return (
+                  <Card
+                    id={elem.id}
+                    key={elem.id}
+                    title={elem.title}
+                    picture={
+                      elem.thumbnail.path + "." + elem.thumbnail.extension
+                    }
+                    description={elem.description}
+                    setFavoriteComics={setFavoriteComics}
+                    favoriteComics={favoriteComics}
+                  />
+                );
+              })
+            ) : (
+              <div className="charging">Chargement en cours</div>
+            )}
+          </div>
         </div>
       </div>
     </div>
