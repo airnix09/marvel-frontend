c2b711441e97ad60bb24a6f96b3ccdc574f3b9c0
airnix09 <airnix09@users.noreply.github.com>
2020-06-04 19:24:28 +0200

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/img/Marvel-Logo.png";
 import "./Header.css";
 
 const Header = ({ context, setContext }) => {
+  const [location, setLocation] = useState("");
   return (
     <div className="header">
       <div className="container">
         <div className="header-top">
-          <img src={Logo} alt="Logo" />
+          <Link to="/">
+            <img src={Logo} alt="Logo" />
+          </Link>
         </div>
       </div>
       <div className="header-bottom">
         <div className="container">
           <ul className="menu">
             <Link to="/">
-              <li>Personnages</li>
+              <li
+                onClick={() => {
+                  setLocation("Personnages");
+                }}
+                className={location === "Personnages" ? "isSelected" : null}
+              >
+                Personnages
+              </li>
             </Link>
             <Link to="/comics">
-              <li>Comics</li>
+              <li
+                onClick={() => {
+                  setLocation("Comics");
+                }}
+                className={location === "Comics" ? "isSelected" : null}
+              >
+                Comics
+              </li>
             </Link>
             <Link to="/favorites">
-              <li>Favoris</li>
+              <li
+                onClick={() => {
+                  setLocation("Favoris");
+                }}
+                className={location === "Favoris" ? "isSelected" : null}
+              >
+                Favoris
+              </li>
             </Link>
           </ul>
         </div>
